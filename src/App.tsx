c54d5c7d9aaf25705c099435b6459d62688db8f9
mc <m.czyzowicz@global4net.com>
2022-01-03 17:22:10 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, {ChangeEvent, FC, useState} from 'react';
+import './App.css';
+
+const App: FC = () => {
+
+    const [task, setTask] = useState<string>("");
+    const [deadline, setDeadline] = useState<number>(0);
+    const [todo, setTodoList] = useState();
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+        if (event.target.name === 'task') setTask(event.target.value)
+        else setDeadline(Number(event.target.value))
+        console.log(task, deadline);
+    };
+
+    return (
+        <div className="App">
+            <div className="header">
+                <div className="inputContainer">
+                    <input type="text" placeholder="Task..." name="task" onChange={handleChange}/>
+                    <input type="number" placeholder="Deadline" name="deadline" onChange={handleChange}/>
+                </div>
+                <button>Add Task</button>
+            </div>
+            <div className="todoList">
+            </div>
+        </div>
+    );
+}
+
+export default App;
